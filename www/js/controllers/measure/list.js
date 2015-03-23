fd7d25d37c0ccf3d83f110c384334d3d62872a8b
nebel <nebel08@gmail.com>
2015-03-23 18:46:59 +0800

    111

--- www/js/controllers/measure/list.js
+++ www/js/controllers/measure/list.js
@@ -3,5 +3,9 @@ Starter_Controller.controller("GymCtrl",['$scope','$location','$rootScope',
 
         $scope.count=0;
 
+         window.plugins.StepListener.getCurrentStep(function(){
+            console.log(arguments);
+        });
+
 
     }]);
